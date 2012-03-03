dfdf100690a3cc83feae82e4d3b0cedbadf95ce0
Dirley Rodrigues <dirleyrls@gmail.com>
2012-03-03 11:21:17 -0300

    Sat Mar  3 11:21:17 2012

--- 2012-02-03-intro/tests.py
+++ 2012-02-03-intro/tests.py
@@ -1,5 +1,8 @@
 import unittest
 
+def gerar_primos_ate(limite):
+    return 2
+
 class TestCrivoEratostenes(unittest.TestCase):
     def teste_primos_ate_2(self):
         self.assertEquals([2], gerar_primos_ate(2))
--- 2012-02-03-intro/tests.py~
+++ 2012-02-03-intro/tests.py~
@@ -1,1 +1,5 @@
-include unittest
+import unittest
+
+class TestCrivoEratostenes(unittest.TestCase):
+    def teste_primos_ate_2(self):
+        self.assertEquals([2], gerar_primos_ate(2))
